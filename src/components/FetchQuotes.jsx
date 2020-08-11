8c630d460f3099690ff18a3d844b92e61f67f288
Taslima <61126921+Taslima26@users.noreply.github.com>
2020-08-11 22:22:35 +0100

    random quote generator fully working

---
+++ src/components/FetchQuotes.jsx
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react';
+
+const FetchQuotes = () => {
+    const [quote, setQuote] = useState({});
+    const [click, setClick] = useState(0);
+
+  useEffect (() => {
+      fetch('https://cors-anywhere.herokuapp.com/https://taslima-quote-sever.glitch.me/quotes/random')
+      .then (res => res.json ())
+      .then (data => {
+       setQuote(data)
+      });
+  }, [click]);
+  return (
+    <div>
+      <div>
+        <h2>{quote.quote}</h2>
+        <h4>{quote.author}</h4>
+
+          </div>
+          <button onClick={() => setClick(click+1)}>Get a new quote!</button>
+    </div>
+  );
+};
+
+export default FetchQuotes;
